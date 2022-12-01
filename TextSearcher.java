1a56022ce0acdacae065b2db7f945e8ae9b3ff2b
oliverocaranza <87118668+oliverocaranza@users.noreply.github.com>
2022-12-01 10:56:17 +0100

    Update TextSearcher.java

--- TextSearcher.java
+++ TextSearcher.java
@@ -5,20 +5,24 @@ import java.util.Scanner;
 
 
 public class TextSearcher {
+	
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner searchWord = new Scanner(System.in);
-		System.out.println("Input the word you want to search for: ");
-		String word = searchWord.next();
-		Scanner searchFile = new Scanner(System.in);
-		System.out.println("Input the file you want to search: ");
-		String file = searchFile.next();
-		Scanner scan = new Scanner(new File(file));
-
-		while(scan.hasNextLine()){
-		    String s = scan.nextLine();
-		    if(s.indexOf(word) != -1){
-		        System.out.println(s);
-		    }
+		Scanner input = new Scanner(System.in);
+		System.out.println("Input here in the form 'Search <word> <file>': ");
+		String input1 = input.next();
+		String[] inputs = input1.split(" ");
+		
+		if (inputs.length == 3 && inputs[0].equals("Search")) {
+			Scanner file = new Scanner(new File(inputs[2]));
+			while(file.hasNextLine()){
+			    String line = file.nextLine();
+			    if(line.contains(inputs[1])){
+			        System.out.println(line);
+			    }
+			}
+		}
+		else {
+			System.out.println("Inpust must be in the requested form.");
 		}
 	}
 }
